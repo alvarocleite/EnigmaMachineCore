39ca6b029bdee90d6d965c55db16cca78ed9326d
Alvaro C Leite <alvaroleite01@gmail.com>
2025-05-17 22:30:42 +0100

    Refactor EnigmaMachine constructor to initialize rotorBox with file paths for rotor and reflector

--- EnigmaMachine/src/EnigmaMachine.cpp
+++ EnigmaMachine/src/EnigmaMachine.cpp
@@ -4,7 +4,11 @@
 #include "../include/EnigmaMachine.hpp"
 
 EnigmaMachine::EnigmaMachine()
-    : rotorBox(3, std::vector<int> {0, 0, 0})
+    : rotorBox(3, std::vector<int> {0, 0, 0},
+              std::vector<std::string> {"../assets/Rotor1.txt", 
+                                        "../assets/Rotor2.txt", 
+                                        "../assets/Rotor3.txt", 
+                                        "../assets/Reflector.txt"})
 {}
 
 EnigmaMachine::~EnigmaMachine(){}
